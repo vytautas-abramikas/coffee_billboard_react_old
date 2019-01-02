cf852590605fcb375736f3735e753188bb106903
Vytautas Abramikas <abramikas@gmail.com>
2019-01-02 17:05:30 +0200

    Updates

---
+++ precache-manifest.40f6c607995660a069763878f84d00e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "490a5a4631a0c789cf84",
+    "url": "/coffee_billboard/static/js/runtime~main.490a5a46.js"
+  },
+  {
+    "revision": "11f6a7a78ad86b58ec59",
+    "url": "/coffee_billboard/static/js/main.11f6a7a7.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/coffee_billboard/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "11f6a7a78ad86b58ec59",
+    "url": "/coffee_billboard/static/css/main.128f38bb.chunk.css"
+  },
+  {
+    "revision": "1be74f65fd896efcb52553d401690c50",
+    "url": "/coffee_billboard/index.html"
+  }
+];
