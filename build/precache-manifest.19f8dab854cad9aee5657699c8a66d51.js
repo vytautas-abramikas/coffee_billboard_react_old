c8b4c67611a011c3c8755c9142fd44542072c162
Vytautas Abramikas <abramikas@gmail.com>
2019-01-02 16:01:14 +0200

    Added build directory

---
+++ build/precache-manifest.19f8dab854cad9aee5657699c8a66d51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4764330e969f9e754439",
+    "url": "/static/js/main.4764330e.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "4764330e969f9e754439",
+    "url": "/static/css/main.128f38bb.chunk.css"
+  },
+  {
+    "revision": "327ead45ddba4c8487cf0b6897f3dad9",
+    "url": "/index.html"
+  }
+];
